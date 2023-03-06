ac19e78b965cd8f077f2115c48be9ad24648e42b
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-06 00:44:27 -0800

    refactor main page App Bar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -9,15 +9,34 @@ class main_page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-    backgroundColor: Color.fromARGB(255, 51, 51, 51),
+    return const Scaffold(
+      appBar: Main_AppBar()
+    );
+  }
+}
+
+class Main_AppBar extends StatelessWidget implements PreferredSize{
+  const Main_AppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+    backgroundColor: const Color.fromARGB(255, 51, 51, 51),
     actions:  [GestureDetector(child: Container(child: const Icon(Icons.search,size: 30,),decoration: BoxDecoration(color:const Color.fromARGB(255, 46, 46, 46),borderRadius: BorderRadius.circular(50) ),width: 50,)),const SizedBox(width: 10,)],
     centerTitle: false,
     title: const Padding(
-      padding:  EdgeInsets.only(left:8.0),
-      child:  Text('Notes',style: TextStyle(fontWeight: FontWeight.bold),),
-    ) ,)
-    );
+    padding:  EdgeInsets.only(left:8.0),
+    child:  Text('Notes',style: TextStyle(fontWeight: FontWeight.bold),),
+    ) ,);
   }
+  
+  @override
+  // TODO: implement child
+  Widget get child => throw UnimplementedError();
+  
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => throw UnimplementedError();
 }
