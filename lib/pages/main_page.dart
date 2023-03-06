8851e37d70d8f7c7cb7d29d5eb77f3e18efa0f2f
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-06 00:23:45 -0800

    Remove debug banner and Build App Bar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+
 class main_page extends StatelessWidget {
   const main_page({super.key});
   static String main_id= 'main page';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Color.fromARGB(255, 51, 51, 51),
+      actions:  [GestureDetector(child: const Icon(Icons.search,size: 30,)),const SizedBox(width: 10,)],
+      centerTitle: false,
+      title: const Padding(
+        padding:  EdgeInsets.only(left:8.0),
+        child:  Text('Notes',style: TextStyle(fontWeight: FontWeight.bold),),
+      ) ,)
+    );
   }
 }
