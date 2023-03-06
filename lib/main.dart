88c230231c6e993aad8b12107d347c53371936c2
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-05 21:45:53 -0800

    putting dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ void main() {
 
 class notes_app extends StatelessWidget {
   const notes_app({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {main_page.main_id:(context) => main_page()},
-      initialRoute: main_page.main_id
+      initialRoute: main_page.main_id,
+      theme: ThemeData.dark() ,
+
     );
   }
 }
