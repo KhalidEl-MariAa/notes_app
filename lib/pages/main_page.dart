730ce0bd34c2883033ed4631afe489e771cd513f
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-06 00:39:32 -0800

    Add Container to search icon

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -11,13 +11,13 @@ class main_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      backgroundColor: Color.fromARGB(255, 51, 51, 51),
-      actions:  [GestureDetector(child: const Icon(Icons.search,size: 30,)),const SizedBox(width: 10,)],
-      centerTitle: false,
-      title: const Padding(
-        padding:  EdgeInsets.only(left:8.0),
-        child:  Text('Notes',style: TextStyle(fontWeight: FontWeight.bold),),
-      ) ,)
+    backgroundColor: Color.fromARGB(255, 51, 51, 51),
+    actions:  [GestureDetector(child: Container(child: const Icon(Icons.search,size: 30,),decoration: BoxDecoration(color:const Color.fromARGB(255, 46, 46, 46),borderRadius: BorderRadius.circular(50) ),width: 50,)),const SizedBox(width: 10,)],
+    centerTitle: false,
+    title: const Padding(
+      padding:  EdgeInsets.only(left:8.0),
+      child:  Text('Notes',style: TextStyle(fontWeight: FontWeight.bold),),
+    ) ,)
     );
   }
 }
