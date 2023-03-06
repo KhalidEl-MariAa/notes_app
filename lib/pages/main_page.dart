873019397948a42e8443cc8c2c7b460cb7323f51
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-05 21:20:34 -0800

    create MaterialApp and main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class main_page extends StatelessWidget {
+  const main_page({super.key});
+  static String main_id= 'main page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
