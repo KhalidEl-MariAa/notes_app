deed9477b7946c6951c89daea299865e2890c481
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-08 06:58:31 -0800

    border of show modal sheet

--- lib/factors/add_note_showbottom.dart
+++ lib/factors/add_note_showbottom.dart
@@ -7,6 +7,15 @@ class Notes_Add extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ;
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
