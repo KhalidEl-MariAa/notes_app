e3536219e01e64c5a6c4c3fff72334f275076537
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-09 07:21:27 -0800

    create note model

---
+++ lib/Models/note_models.dart
@@ -0,0 +1,15 @@
+// ignore: camel_case_types
+class note_model{
+
+  String title;
+  String subtitle;
+  int color;
+  String date;
+
+  note_model({
+    required this.title
+    ,required this.subtitle,
+    required this.color
+    ,required this.date
+    });
+}
