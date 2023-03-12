b78c08fab550f39a91648c508cc046b59c216c88
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-12 07:03:36 -0700

    add build runner packages

--- lib/Models/note_models.dart
+++ lib/Models/note_models.dart
@@ -1,9 +1,20 @@
 // ignore: camel_case_types
-class note_model{
+import 'package:hive/hive.dart';
+part 'note_models.g.dart';
 
+@HiveType(typeId: 0)
+class note_model extends HiveObject{
+
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String subtitle;
+
+  @HiveField(2)
   int color;
+  
+  @HiveField(3)
   String date;
 
   note_model({
