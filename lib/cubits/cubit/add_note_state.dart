0f7542ffe7c8c866a5e5c503ca8e12a19aeddc4a
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-15 04:53:18 -0700

    Initialize Hive and Create add note cubit

---
+++ lib/cubits/cubit/add_note_state.dart
@@ -0,0 +1,19 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNotefailure extends AddNoteState {
+  String error;
+
+  AddNotefailure({required this.error});
+  
+}
+
+class AddNoteLoading extends AddNoteState {}
+
+
