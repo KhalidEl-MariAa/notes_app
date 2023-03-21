be2635a2d358d254612ded66008a723cd968791d
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-03-21 05:06:18 -0700

    add circle indicator inside button,delete modalHUD

--- lib/factors/custom_button.dart
+++ lib/factors/custom_button.dart
@@ -1,19 +1,27 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants/constant.dart';
+import 'package:notes_app/cubits/cubit/add_note_cubit.dart';
 
 class MyButton extends StatelessWidget {
-   MyButton({this.onPressed});
+  MyButton({this.onPressed});
   void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed : onPressed 
-    
-    , child: Text('Add',style: TextStyle(color: Colors.black),)
-
-    ,style: ElevatedButton.styleFrom(fixedSize: const Size(360, 50),backgroundColor: color),
+    return BlocBuilder<AddNoteCubit, AddNoteState>(
+      builder: (context, state) {
+        return ElevatedButton(
+          onPressed: onPressed,
+          child: state is AddNoteLoading? SizedBox(child: CircularProgressIndicator(color: Colors.black,),height: 24,width: 24,) : 
+          Text(
+            'Add',
+            style: TextStyle(color: Colors.black),)
+          ,
+          style: ElevatedButton.styleFrom(
+              fixedSize: const Size(360, 50), backgroundColor: color),
+        );
+      },
     );
   }
 }
+
